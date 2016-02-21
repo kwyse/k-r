adbdf94619e38bf68a1a9590e207f4f8537544f0
Krishan Wyse <kwysek@gmail.com>
2016-02-21 18:36:57 +0000

    Update run script to be more robust

--- run.sh
+++ run.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-command="gcc ${1}.c && ./a.out && rm ./a.out"
-eval $command
+function cleanup {
+  if [[ -f a.out ]]; then rm ./a.out; fi
+}
+trap cleanup EXIT
+
+if [[ $# -ne 2 ]]; then
+  echo "Usage: run.sh <chapter> <exercise>"
+  exit -1
+fi
+
+filename="$1-$2.c"
+if [[ ! -f $filename ]]; then
+  echo -e "Cannot find file $filename\nExiting..."
+  exit -1
+fi
+
+gcc $filename
+./a.out
