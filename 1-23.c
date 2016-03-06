fe7ce7e37f7ff83a9981574e9551a90aacfe03e5
Krishan Wyse <kwysek@gmail.com>
2016-03-06 23:08:32 +0000

    Fix 1-23

--- 1-23.c
+++ 1-23.c
@@ -62,20 +62,26 @@ void remove_comments(char str[]) {
 
     if (str[i] == '*' && str[i + 1] == '/') {
       in_cblock = false;
+      memmove(str + i, str + i + 3, strlen(str + i + 3) + 1);
       puts("CB-");
+    }
+
+    if (in_cblock && !in_squote && !in_dquote) {
+      memmove(str + i, str + i + 1, strlen(str + i + 1) + 1);
+      i--;
     }
 
     //* Another commetn */
 
     //if (in_cblock && !in_squote && !in_dquote)
       //str[i] = '@';
-      //strshft(str + i, 1);
+   //strshft(str + i, 1);
       //memmove(&str[i], &str[i + 1], strlen(&str[i]));
-      rmsubstr(str, 14, 20);
+      //rmsubstr(str, 14, 20);
     //&(str+i) = memmove(str + i, str + i + 1, strlen(str));
       //rmsubstr(str, strstr(str, "/* comment */"), 13);
       // memmove(strstr(str, "/* comment */"), strstr(str, "/* comment */") + 13, strlen(str) - 26);
-    len--;
+    //    len--;
   }
 }
 
@@ -83,13 +89,31 @@ void rmsubstr(char str[], size_t start, size_t end) {
   memmove(str + start, str + end, strlen(str) + start);
 }
 
+void decomment(char str[]) {
+  int start = -1, end = -1;
+
+  for (size_t i = 0; str[i] != '\0'; i++) {
+    if (str[i] == '/' && str[i + 1] == '*')
+      start = i;
+
+    if (str[i] == '*' && str[i + 1] == '/')
+      end = i + 2;
+  }
+
+  if (start != -1 && end != -1) {
+    printf("Removing comment beween indexes %d and %d\n", start, end);
+    memmove(str + start, str + end, strlen(str + end) + 1);
+  }
+}
+    //while ((c = getc()) != EOF) 
+
 int main() {
   char* input = getstr("test");
   char str[strlen(input)];
   strcpy(str, input);
   free(input);
 
-  remove_comments(str);
+  //remove_comments(str);
   puts(str);
 
   char* s = "This is a very long string";
@@ -100,11 +124,13 @@ int main() {
   puts("VVV");
   //puts(s);
   //strshft(s, 1);
-  memmove(ss + 4, ss + 4 + 3, strlen(ss) - 4 - 3 + 1);
+  //memmove(ss + 4, ss + 4 + 3, strlen(ss) - 4 - 3 + 1);
   puts(ss);
 
-  rmsubstr(ss, 2, 4);
-  puts(ss);
+  //rmsubstr(ss, 2, 4);
+  //decomment(str);
+  remove_comments(str);
+  puts(str);
 
   /* This comment with be removed */
 
