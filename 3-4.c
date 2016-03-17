e4896e3f52e19c3e95484f9594fb0fe3fad9e342
Krishan Wyse <kwysek@gmail.com>
2016-03-17 20:09:23 +0000

    Finish exercise

--- 3-4.c
+++ 3-4.c
@@ -1,33 +1,46 @@
 // 3.4 : itoa
 
+#include <math.h>
 #include "header.h"
 
 void itoa(int n, char s[]);
 
 void itoa(int n, char s[]) {
-  int sign = n;
-  if (n < 0)
-    n = -n;
+  bool isneg = n < 0;
+  if (isneg)
+    n = -(n + 1);
 
-  int i = 0;
-  do
+  size_t i = 0;
+  s[i++] = (n % 10) + (isneg ? 1 : 0) + '0';
+  while ((n /= 10) > 0)
     s[i++] = (n % 10) + '0';
-  while ((n /= 10) > 0);
 
-  if (sign < 0)
+  if (isneg)
     s[i++] = '-';
-  s[i] = '\0';
 
+  s[i] = '\0';
   reverse(s);
 }
 
 int main() {
   char output[100];
+  itoa(-8, output);
+  puts(output);
   itoa(-3344, output);
   puts(output);
   itoa(-2147483647, output);
   puts(output);
+  itoa(2147483647, output);
+  puts(output);
   itoa(-2147483648, output);
+  puts(output);
+  itoa(0, output);
+  puts(output);
+  itoa(1, output);
+  puts(output);
+  itoa(-1, output);
+  puts(output);
+  itoa(-21, output);
   puts(output);
 
   return 0;
--- header.h
+++ header.h
@@ -6,8 +6,27 @@
 #include <string.h>
 
 void putd(int d);
+size_t getlnlen(char* line);
+void reverse(char str[]);
 
 // Prints a signed integer to stdout
 void putd(int d) {
   printf("%d\n", d);
+}
+
+// Returns the length of a line of characters
+size_t getlnlen(char* line) {
+  size_t i;
+  for (i = 0; line[i] != '\n' && line[i] != '\0'; i++);
+  return i;
+}
+
+// Reverses a string in place
+void reverse(char str[]) {
+  size_t len = getlnlen(str);
+  for (size_t i = 0, j = len - 1; (i < (len / 2)) && (j >= (len / 2)); i++, j--) {
+    char tmp = str[i];
+    str[i] = str[j];
+    str[j] = tmp;
+  }
 }
